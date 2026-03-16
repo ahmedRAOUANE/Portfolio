cc426d6b97a305f002368b4d1da9946c21ecd50e
ahmed RAOUANE <ahmedraouane30@gmail.com>
2026-03-16 14:26:50 +0100

    build the resume addition logic

--- src/actions/resumes.ts
+++ src/actions/resumes.ts
@@ -1,5 +1,12 @@
 "use server";
 
+import { baseUrl } from "@/utils/constansts";
+import { insertIn } from "@/utils/data/data-cruds";
+import { Roles } from "@/utils/types/roles";
+import { Routes } from "@/utils/types/routes";
+import { checkForError } from "@/utils/validations";
+import { revalidatePath } from "next/cache";
+
 type ResumeType = {
     resumeName: string;
     description: string;
@@ -16,4 +23,21 @@ export const getResumes = async (): Promise<ResumeType[]> => {
         isActive: false,
         createdAt: "21/3/26"
     }]
+}
+
+export const addResume = async (formData: FormData) => {
+    try {
+        const payload = {
+            resumeName: formData.get("resumeName"),
+            description: formData.get("description"),
+            link: formData.get("link"),
+        };
+
+        const { success: insertSuccess, message: insertErrorMessage } = await insertIn("resumes", payload, "*", Roles.admin)
+        checkForError(insertSuccess, `insertion Error > POST > insertIn: ${insertErrorMessage || "failed to insert in table: resumes"}`, "object", 500)
+
+        revalidatePath(`${baseUrl}/${Routes.admin}/resumes`);
+    } catch (err) {
+        console.log("error from add resume server action: ", err);
+    }
 }
--- src/components/admin-components/add-resume.tsx
+++ src/components/admin-components/add-resume.tsx
@@ -1,88 +1,25 @@
 "use client";
 
-import { baseUrl } from "@/utils/constansts";
-import { CustomResponse } from "@/utils/types/response";
-import { useRouter } from "next/navigation";
-import { FormEvent, useRef, useState } from "react";
+import { addResume } from "@/actions/resumes";
 import ActionBtn from "../action-btn";
 
 const AddResume = () => {
-    const nameRef = useRef<HTMLInputElement>(null);
-    const descriptionRef = useRef<HTMLTextAreaElement>(null);
-    const linkRef = useRef<HTMLInputElement>(null);
-    const isActiveRef = useRef<HTMLInputElement>(null);
-
-    const [pending, setPending] = useState(false);
-    const [imagePreview, setImagePreview] = useState<string | null>(null);
-
-    const router = useRouter();
-
-    const resetForm = () => {
-        if (nameRef.current) nameRef.current.value = "";
-        if (descriptionRef.current) descriptionRef.current.value = "";
-        if (linkRef.current) linkRef.current.value = "";
-        if (isActiveRef.current) isActiveRef.current.checked = true;
-        if (imagePreview) setImagePreview(null);
-    }
-
-    const handleAddResume = async (e: FormEvent): Promise<CustomResponse> => {
-        e.preventDefault()
-
-        setPending(true);
-
-        const formData = new FormData();
-        formData.append("resumeName", nameRef.current?.value || "");
-        formData.append("description", descriptionRef.current?.value || "");
-        formData.append("link", linkRef.current?.value || "");
-        formData.append("isActive", (isActiveRef.current?.checked ? "true" : "false"))
-        formData.append("createdAt", Date.now().toString());
-
-        try {
-            const res = await fetch(`${baseUrl}/api/resume`, {
-                method: "POST",
-                body: formData,
-                credentials: "include"
-            })
-
-            await res.json();
-            if (!res.ok) {
-                return {
-                    success: false,
-                    message: "client: failed to add resume"
-                }
-            }
-
-            resetForm();
-            setPending(false);
-
-            router.refresh();
-
-            return {
-                success: true,
-                message: "resume added successfully"
-            }
-        } catch (error) {
-            setPending(false);
-            return {
-                success: false,
-                message: error instanceof Error ? error.message : "Failed to add resume"
-            }
-        }
-    }
-
     return (
         <div className="w-full">
             <h2 className="text-2xl font-bold mb-6 text-primary">Add New Resume</h2>
 
-            <form className="w-full flex flex-col gap-6" onSubmit={handleAddResume}>
+            <form
+                className="w-full flex flex-col gap-6"
+                action={addResume}
+            >
                 <div className="bg-primary/5 p-3 rounded-lg border border-primary/30 focus-within:border-primary/50 transition-all duration-300">
-                    <label htmlFor="projectName" className="hidden">Resume Name</label>
+                    <label htmlFor="resumeName" className="hidden">Resume Name</label>
                     <input
-                        ref={nameRef}
-                        placeholder="Project Name"
+                        // ref={resNameRef}
+                        placeholder="Resume Name"
                         type="text"
-                        id="projectName"
-                        name="projectName"
+                        id="resumeName"
+                        name="resumeName"
                         required
                         className="w-full bg-transparent text-foreground focus:outline-none placeholder:text-light/50"
                     />
@@ -91,7 +28,7 @@ const AddResume = () => {
                 <div className="bg-primary/5 p-3 rounded-lg border border-primary/30 focus-within:border-primary/50 transition-all duration-300">
                     <label htmlFor="description" className="hidden">Resume Description</label>
                     <textarea
-                        ref={descriptionRef}
+                        // ref={descriptionRef}
                         placeholder="Description"
                         id="description"
                         name="description"
@@ -104,7 +41,7 @@ const AddResume = () => {
                 <div className="bg-primary/5 p-3 rounded-lg border border-primary/30 focus-within:border-primary/50 transition-all duration-300">
                     <label htmlFor="link" className="hidden">Resume Link/URL</label>
                     <input
-                        ref={linkRef}
+                        // ref={linkRef}
                         placeholder="Project Link/URL"
                         type="url"
                         id="link"
@@ -114,10 +51,9 @@ const AddResume = () => {
                     />
                 </div>
 
-                <div className="mb-6 flex items-center justify-start gap-2">
+                {/* <div className="mb-6 flex items-center justify-start gap-2">
                     <input
                         defaultChecked={true}
-                        ref={isActiveRef}
                         type="checkbox"
                         id="isActive"
                         name="isActive"
@@ -126,10 +62,10 @@ const AddResume = () => {
                     <label htmlFor="isActive" className="text-light">
                         Show this resume
                     </label>
-                </div>
+                </div> */}
 
                 <ActionBtn
-                    pending={pending}
+                    // pending={pending}
                     action={{ state1: "Save Resume", state2: "Saving..." }}
                     className="bg-primary/30 hover:bg-primary/40 text-foreground p-3 rounded-lg transition-all duration-300"
                     type="submit"
